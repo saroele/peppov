eca2ad93d01e3e42617146323cfcb2878d4a30fe
Roel De Coninck <roel.deconinck@3e.eu>
2013-09-04 01:23:04 +0200

    First prioritization seems to work

--- peppov.py
+++ peppov.py
@@ -24,41 +24,18 @@ class Project(HasTraits):
     name = Str
     comment = Str
     tags = List(Str, label='all tags')
-    singletag = Str(desc='Tag to be added or removed', label='tag')
+    
     fun = Range(0,5)
     dringendheid = Range(0,5)
     belang = Range(0,5)
     goeiedaad = Range(0,5)
     plieslies = Range(0,5)
     
-    add = Button('Add tag')
-    remove = Button('Remove tag')
-    
-    view_tabs = View(Group(
-                        Item('projectnumber'),
-                        Item(name='name'),
-                        Item(name='comment'),
-                        Item('tags', editor=ListStrEditor(auto_add=True)),
-                        Item('singletag'),
-                        UItem('add'),
-                        UItem('remove'),
-                        label='tags'),
-                    Group(Item('fun'),
-                        Item('dringendheid'),
-                        Item('belang'),
-                        Item('goeiedaad'),
-                        Item('plieslies'),
-                        label='categories')
-                       
-                       )
-                       
+                           
     view_all = View(    Item('projectnumber'),
                         Item(name='name'),
                         Item(name='comment'),
-                        Item('tags', editor=ListStrEditor(auto_add=True)),
-                        Item('singletag'),
-                        UItem('add'),
-                        UItem('remove'),
+                        Item('tags', editor=CSVListEditor()),
                         Item('fun'),
                         Item('dringendheid'),
                         Item('belang'),
@@ -84,23 +61,20 @@ class Project(HasTraits):
         
     def has_tag(self, tag):
         return tag in self.tags
+        
+
 
 
 class Portfolio(HasTraits):
     """Class to manage and analyse a portfolio of projects"""
     
     all_tags = [] #list of possible tags for the Projects
-    all_properties = [] #list of all properties (values between 0 and 5)    
+    all_properties = ['fun','belang','dringendheid','goeiedaad','plieslies'] #list of all properties (values between 0 and 5)    
     current_project = Int(default_value=1)   
     projects = List(Project)  # 
     selection = List(Project) # selection of projects, based on tags
     repr_projects = Property(List, depends_on = 'projects')    
     repr_selection = Property(List, depends_on = 'selection')
-    
-    to_plot_x = Str()
-    to_plot_y = Str()
-    x = Property(Array, depends_on=['to_plot_x', 'property_values'])
-    y = Property(Array, depends_on=['to_plot_y', 'property_values'])
     
     #dictionary of property values, property:list of values
     property_values = Property(Dict, depends_on = "selection")  
@@ -110,6 +84,22 @@ class Portfolio(HasTraits):
     button_add = Button("Add project")
     button_edit = Button("Edit project")
     button_filter = Button("Filter projects")    
+    
+    # Analysis
+    to_plot_x = Str()
+    to_plot_y = Str()
+    x = Property(Array, depends_on=['to_plot_x', 'property_values'])
+    y = Property(Array, depends_on=['to_plot_y', 'property_values'])
+    # sliders for the analysis    
+    fun = Range(0,5)
+    dringendheid = Range(0,5)
+    belang = Range(0,5)
+    goeiedaad = Range(0,5)
+    plieslies = Range(0,5)    
+    
+    priority_projects = Property(List, depends_on = all_properties + ['selection'])
+    
+    
     
     def _get_property_values(self):
         property_values = {}
@@ -124,39 +114,83 @@ class Portfolio(HasTraits):
         return [str(p.projectnumber) + ' - ' + p.name for p in self.selection]
         
     def _get_x(self):
-        return np.ndarray(self.property_values.get(self.to_plot_x, np.zeros(len(self.selection))))
+        return np.array(self.property_values.get(self.to_plot_x, np.zeros(len(self.selection))))
         
     def _get_y(self):
-        return np.ndarray(self.property_values.get(self.to_plot_y, np.zeros(len(self.selection))))
+        return np.array(self.property_values.get(self.to_plot_y, np.zeros(len(self.selection))))
     
     view = View(
-                Item('repr_projects', label='Projects',editor=ListStrEditor(editable=False)),
-                UItem('button_add'),
-                Item('current_project', label='Project to edit'),
-                UItem('button_edit'),                
-                Item('tags_to_include', editor=CSVListEditor()),
-                Item('tags_to_exclude', editor=CSVListEditor()),
-                UItem('button_filter'),
-                Item('repr_selection', label='Selection of projects', editor=ListStrEditor(editable=False)),
-                ChacoPlotItem("x","y", type='scatter',
-                                y_bounds=(0.,6.),y_auto=False,
-                                x_bounds=(0.,6.),x_auto=False,
-                                resizable=True, show_label=False,
-                                x_label=to_plot_x.value, y_label=to_plot_y.value, title=""),
+                Group(
+                    Item('repr_projects', label='Projects',editor=ListStrEditor(editable=False)),
+                    UItem('button_add'),
+                    Item('current_project', label='Project to edit'),
+                    UItem('button_edit'),                
+                    Item('tags_to_include', editor=CSVListEditor()),
+                    Item('tags_to_exclude', editor=CSVListEditor()),
+                    UItem('button_filter'),
+                    Item('repr_selection', label='Selection of projects', editor=ListStrEditor(editable=False)),
+                    label='Projects'),
+                Group(
+                    Item('to_plot_x'),
+                    Item('to_plot_y'),
+                    ChacoPlotItem("x","y", type='scatter',
+                                    y_bounds=(0.,6.),y_auto=False,
+                                    x_bounds=(0.,6.),x_auto=False,
+                                    resizable=True, show_label=False,
+                                    x_label=to_plot_x.value, y_label=to_plot_y.value, title=""),
+                    label='Analysis'),
+                Group(
+                    Item('fun'),
+                    Item('dringendheid'),
+                    Item('belang'),
+                    Item('goeiedaad'),
+                    Item('plieslies'),
+                    Item('priority_projects', label='Priority projects',editor=ListStrEditor(editable=False)),
+                    label='Priorities'),
                 resizable=True
                 )
     
     
-    def _get_new_projectnumber(self):
-        return 1 + self.projects[-1].projectnumber
+    def _get_priority_projects(self):
+        """Sort the projects based on their properties and the sensitivities
+        for each priority"""
         
+        #pdb.set_trace()        
+        priority = np.zeros(len(self.selection))
+        for prop in self.all_properties:
+            #multiply the array of values with the weight, add to previous result
+#            print 'first operand = ', getattr(self, prop)             
+#            print 'type of first operand', type(getattr(self, prop))  
+#            print 'second operand = ', self.property_values[prop]
+#            print 'type of second operand', type(self.property_values[prop])
+            priority += getattr(self, prop) * np.array(self.property_values[prop])
+            
+        sorted_projects = sorted(zip(priority, self.selection), reverse=True)
+        return [str(project.projectnumber) + ' - ' + project.name + \
+            ' - (priority = {})'.format(p) for (p, project) in sorted_projects]
+
+
+    
+    def _get_new_projectnumber(self):
+        try:
+            return 1 + self.projects[-1].projectnumber
+        except:
+            return 1
                
-    def add_project(self):
-        """Add a project to the dictionary of projects"""
+    def add_project_GUI(self):
+        """Add a project to the list of projects and open GUI"""
         projectnumber = self._get_new_projectnumber()
         p = Project(projectnumber=projectnumber)
         p.configure_traits(view='view_all')
         self.projects.append(p)
+        
+    def add_project(self, **kwargs):
+        """Add a project to the dictionary of projects with **kwargs"""
+        projectnumber = self._get_new_projectnumber()
+        p = Project(projectnumber=projectnumber, **kwargs)
+        
+        self.projects.append(p)
+        self.filter_projects(incl=self.tags_to_include, excl=self.tags_to_exclude) 
         
     def edit_project(self, projectnumber):
         """Edit a project"""
@@ -169,6 +203,10 @@ class Portfolio(HasTraits):
         self.filter_projects(incl=self.tags_to_include, excl=self.tags_to_exclude) 
         
         
+    def _projects_changed(self):
+        """Always update the selection when the projects change"""
+        self.filter_projects(incl=self.tags_to_include, excl=self.tags_to_exclude)
+    
     def filter_projects(self, incl=[], excl=[]):
         """Filter projects and update self.selection"""
         
@@ -200,7 +238,7 @@ class Portfolio(HasTraits):
                 
             
     def _button_add_fired(self):
-        self.add_project()
+        self.add_project_GUI()
         
     def _button_edit_fired(self):
         self.edit_project(self.current_project)
@@ -210,7 +248,36 @@ class Portfolio(HasTraits):
     def _button_filter_fired(self):
         self.filter_projects(incl=self.tags_to_include, excl=self.tags_to_exclude)            
         
+    def read_csv(self, filename):
+        """Read projects from a csv file"""
         
+        import pandas as pd
+        df = pd.read_csv(filename, sep=';')
+        for id, project in df.iterrows():
+            kwargs = project.to_dict()
+            kwargs['tags'] = kwargs['tags'].split(',')
+            if type(kwargs['comment']) is not str:
+                kwargs['comment'] = ''
+            self.add_project(**kwargs)
+            
+    def save_csv(self, filename):
+        
+        f = open(filename, 'w')
+        # write the header
+        columns = ['name','fun','belang','dringendheid','goeiedaad','plieslies','tags','comment']
+        f.write(';'.join(columns))
+        f.write('\n')
+        
+        # write the lines, one per project
+        for project in self.projects:
+            line = ';'.join([str(getattr(project, c)) for c in \
+                ['name','fun','belang','dringendheid','goeiedaad','plieslies']])
+            line += ';'
+            line += ','.join(getattr(project, 'tags'))
+            line += ';' + project.comment
+            line += '\n'
+            f.write(line)
+        f.close()
     
 
 
